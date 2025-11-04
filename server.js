dd4aa3707b7d0e29fb4129794065f4e4a023329b
mobailinsiders-stack <mobailinsiders@gmail.com>
2025-11-04 11:47:16 +0530

    1
    1

---
+++ server.js
@@ -0,0 +1,14 @@
+{
+  "name": "qikink-firestore-sync",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "axios": "^1.4.0",
+    "express": "^4.18.2",
+    "body-parser": "^1.20.2",
+    "firebase-admin": "^11.11.0"
+  }
+}
